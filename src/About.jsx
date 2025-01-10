c33a383903f2443f353c3f40de372272f74b5677
रजत | Rajat Kumar <97496281+Rajat0075@users.noreply.github.com>
2025-01-10 17:41:48 +0530

    Add files via upload

---
+++ src/About.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+function About() {
+  return (
+
+    <div>
+      <Navbar />
+      <br/>
+      <div class="container header">
+        <h2>About Us</h2>
+        <p>Home | About Us</p>
+        <hr/>
+      </div>
+      {/*about */}
+      <br />
+      <div class="container-lg">
+        <p>Hello, Dear friends, Welcome to WebisteName1 also, we are happy you want to know something more about our site</p>
+        <p>So, basically, nowadays people are more dependent on online products and services that's why we also, take
+          forward a step to help you.</p>
+        <p>Our first wish is to provide you with a better solution to solve your problem. So, kindly if you don't get any
+          solution then mention it in the comment section.</p>
+        <p>Also, we are trying to provide fresh & latest content that provides you ideas about all updated information
+          that's happening in the world.</p>
+        <p>In the below section you can get more ideas about our site like our website category and content category.</p>
+        <p>If you have additional questions or require more information about our About Us Page, do not hesitate to contact
+          us through email at <b>website@gmail.com</b></p>
+        <h2><b>What is Our Goal?</b></h2>
+        <p>There are millions of websites created every day, also, there is much fake content spread on the internet.</p>
+        <p>So, Our main goal is to provide you with 100% Original and Safe content that provides you a great and better
+          experience on the world wide web.</p>
+        <p>We mainly focus on our service so and improving it regularly to provide a better user experience to all users.
+        </p>
+        <p>Basically, we focus on the Education niche so, our main priority is to search for new content and present it in
+          front of you to learn something new. </p>
+        <h3><b>What is our Service?</b></h3>
+        <p>We are mainly focused on the Education category so, we provide Education related content if you are interested in
+          the Education category then you can visit daily to get more latest information.</p>
+        <p>Website helps to provides higher education to customer </p>
+        <p>On our website WebisteName1 you get can all Education related information also, we focus on many other categories
+          and we hope you like also, the content of other categories that are maintained on our website. So, you can visit
+        our website homepage to know all category details here you can visit our homepage Click here --><a
+            href="website.com">WebisteName1</a>.</p>
+        <p>Also, we provide a Notification update service you can join by email and other Social Media Platforms, and all
+          Links you can get on the Homepage visit now. <a href="website.com">WebisteName1</a></p>
+        <h3><b>About E-Library</b></h3>
+        <p>As you can see already we mention what is our goal and Service again we repeat that we mainly focus on Education
+          Category to help people.</p>
+        <p>This Website is Created By WebisteName1 to help people because many people are still spending hours of time to
+          get exact information so, this is the only motive to create <a href="website.com">WebisteName1</a> to help
+          people
+          and provide them a better web experience. Also, This About Us Page is Generated With <a
+            href="https://soumyahelp.com">Soumya help</a>.</p>
+        <p>Now, the time is to know about the Admin Details of this website, so, now we have to go down to know about Admin
+          details.</p>
+        <h3><b>Admin's Statement for E-Library</b></h3>
+        <p>As per my point of view, there are many people who visit the internet to get some information but 90% of the time
+          they get wrong information so, the first priority of our website WebisteName1 is to provide 100% legit and
+          accurate information to our users, Also, I hope my dream comes true one day, and our website will provide
+          Original Content to provide a better user experience. So, From my Side thanks for visiting our website.</p>
+        <h3><b>Admin's Contact Information</b></h3>
+        <p>Hi, this WebsiteAuthor, in the above paragraph you know about the website properly and now I am going to provide
+          my contact details.</p>
+        <p>If you have any problem & suggestions for this website then you can contact me by using following contact
+          details.</p>
+
+        <caption>
+          <table class="table table-primary">
+            <tbody>
+              <tr>
+                <td scope="col">Name</td><td scole="col"><strong>Rajat Kumar</strong></td>
+              </tr>
+              <tr><td scole="col">Email</td><td scole="col"><strong>elibrary@gmail.com</strong></td></tr>
+              <tr><td scole="col">Facebook Id</td><td scole="col"><a href="FBID23"><strong>Facebook</strong></a></td></tr>
+              <tr>
+                <td scole="col">Twitter Id</td>
+                <td scole="col"><strong><a href="TwitterID12">Twitter</a></strong>/</td>
+              </tr>
+              <tr>
+                <td scole="col">Instagram Id</td>
+                <td scole="col"><a href="InstagramID23"><strong>Instagram</strong></a></td>
+              </tr>
+            </tbody>
+          </table>
+        </caption>
+
+        <p>These details are my personal Account details if you want to contact me then you can contact me by the above
+          platform.</p>
+        <p>Finally, this is our complete about us page about details are showing what is the motive to create WebisteName1.
+          Also, this page is generated by Soumya Help <a href="https://soumyahelp.com/about-us-generator/">About Us
+            Generator</a></p>
+        <p>If you want to contact us then you can email us at website@gmail.com also, you can contact us by our contact us
+          form. Go to homepage <a href="website.com">WebisteName1</a><a
+            href="https://soumyahelp.com/about-us-generator/">.</a></p>
+        <p>"Thanks for visiting our About Us Page"</p>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About
